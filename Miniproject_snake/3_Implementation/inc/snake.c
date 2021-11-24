22a6748b8fce6b58ef1e1c58024bec00e348b68d
padma510 <94212251+padma510@users.noreply.github.com>
2021-11-24 18:20:19 +0530

    Update snake.c

--- Miniproject_snake/3_Implementation/inc/snake.c
+++ Miniproject_snake/3_Implementation/inc/snake.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+#include <stdlib.h>
+
+int main()
+{
+
+    char key;
+
+    Print();
+
+    system("cls");
+
+    load();
+
+    length=5;
+
+    head.x=25;
+
+    head.y=20;
+
+    head.direction=RIGHT;
+
+    Boarder();
+
+    Food(); //to generate food coordinates initially
+
+    life=3; //number of extra lives
+
+    bend[0]=head;
+
+    Move();   //initialing initial bend coordinate
+
+    return 0;
+
+}
